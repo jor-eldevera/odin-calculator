b614cdfd8470dbc46e70aaba79d9cbe8e52f6ac9
jor-eldevera <jorell.devera@gmail.com>
2022-07-22 18:26:27 -0700

    Make digits change display

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
+const display = document.querySelector(".display");
+let displayText = "";
+// display.textContent = displayText;
+
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const zero = document.querySelector(".zero");
+
+function addNumber(num) {
+    if (displayText.length < 15) {
+        displayText += num;
+        display.textContent = displayText;
+    }
+}
+
+one.addEventListener("click", () => {
+    addNumber("1");
+});
+
+two.addEventListener("click", () => {
+    addNumber("2");
+});
+
+three.addEventListener("click", () => {
+    addNumber("3");
+});
+
+four.addEventListener("click", () => {
+    addNumber("4");
+});
+
+five.addEventListener("click", () => {
+    addNumber("5");
+});
+
+six.addEventListener("click", () => {
+    addNumber("6");
+});
+
+seven.addEventListener("click", () => {
+    addNumber("7");
+});
+
+eight.addEventListener("click", () => {
+    addNumber("8");
+});
+
+nine.addEventListener("click", () => {
+    addNumber("9");
+});
+
+zero.addEventListener("click", () => {
+    addNumber("0");
+});
+
 function add(a, b) {
     return a + b;
 }
